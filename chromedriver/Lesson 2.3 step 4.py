1c4337df5c4f5917efb0f32ef2f0eb18a56c2a94
vlaf27449 <33995288+vlaf27449@users.noreply.github.com>
2021-09-13 10:57:57 +0700

    Lesson 2.3 step 6

--- chromedriver/Lesson 2.3 step 4.py
+++ chromedriver/Lesson 2.3 step 4.py
@@ -7,19 +7,19 @@ import math
 
 try:
      browser = webdriver.Chrome()
-     url = 'http://suninjuly.github.io/redirect_accept.html'
+     url = 'http://suninjuly.github.io/alert_accept.html'
      browser.get(url)
 
-     button = browser.find_element_by_css_selector("[type='submit']")
+     button = browser.find_element_by_css_selector('.btn')
      button.click()
 
-     new_window = browser.window_handles[1]
-     browser.switch_to.window(new_window)
+     confirm = browser.switch_to.alert
+     confirm.accept()
 
      x = int(browser.find_element_by_css_selector('#input_value').text)
      result = str(math.log(abs(12*math.sin(x))))
 
-     input = browser.find_element_by_css_selector('.form-control')
+     input = browser.find_element_by_css_selector('#answer')
      input.send_keys(result)
 
      button = browser.find_element_by_css_selector('.btn')
