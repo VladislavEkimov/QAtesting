a2e501af5b8f8421f12e74f9d9061409a7074f51
vlaf27449 <33995288+vlaf27449@users.noreply.github.com>
2021-09-14 16:32:24 +0700

    test

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -1,2 +1,48 @@
-def test_substring(full_string, substring):
-    assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
+import time
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common import keys
+
+
+class TestYandex(unittest.TestCase):
+
+    def testInput(self):
+        browser = webdriver.Chrome()
+        link = 'https://yandex.ru/'
+        browser.get(link)
+        if browser.find_elements_by_xpath('//*[@id="text"]'):
+            input = True
+        else:
+            input = False
+        self.assertEqual(input, True, 'No input field')
+
+    def testSearch(self):
+        browser = webdriver.Chrome()
+        link = 'https://yandex.ru/'
+        browser.get(link)
+        input = browser.find_element_by_xpath('//*[@id="text"]')
+        input.send_keys('тензор')
+
+        if browser.find_element_by_css_selector("[role='listbox']"):
+            suggest = True
+        else:
+            suggest = False
+        self.assertEqual(suggest, True, 'No suggest field')
+
+    def testLink(self):
+        browser = webdriver.Chrome()
+        link = 'https://yandex.ru/'
+        browser.get(link)
+        input = browser.find_element_by_xpath('//*[@id="text"]')
+        input.send_keys('тензор')
+        input.send_keys(keys.Keys.ENTER)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
